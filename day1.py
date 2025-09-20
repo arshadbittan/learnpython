ad87a6c1e34bb62da9dac3e67c4dc7490bb94f84
arshadbittan <arshadbittan1999@gmail.com>
2025-09-20 19:14:59 +0530

    Add Day 1 Python practice

---
+++ day1.py
@@ -0,0 +1,21 @@
+print("hello")
+
+A= 5  #A IS THE VARIABLE
+B=10  #B IS THE VARIABLE
+
+#VARIABLES ARE THE CONTAINERS THAT STORES VALUES
+print(A+B)
+
+print(A*B)
+
+print(A/B)
+
+print(A%B)
+
+print(A**B)
+
+print(A//B)
+
+print(A-B)
+
+print(B-A)
