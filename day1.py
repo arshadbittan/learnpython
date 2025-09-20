3c51c44e4600b27ff8bbd69ea2a46da1b73581bc
arshadbittan <arshadbittan1999@gmail.com>
2025-09-20 19:24:57 +0530

    Update Day 1 Python practice

--- day1.py
+++ day1.py
@@ -4,18 +4,18 @@ A= 5  #A IS THE VARIABLE
 B=10  #B IS THE VARIABLE
 
 #VARIABLES ARE THE CONTAINERS THAT STORES VALUES
-print(A+B)
+print(A+B) #output 15
 
-print(A*B)
+print(A*B)  #output 50
 
-print(A/B)
+print(A/B)  #output 0.5
 
-print(A%B)
+print(A%B)  #output 5
 
-print(A**B)
+print(A**B) #output 9765625
 
-print(A//B)
+print(A//B)  #output 0
 
-print(A-B)
+print(A-B)   #output -5
 
-print(B-A)
+print(B-A)   #output 5
